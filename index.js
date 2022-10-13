0e93cd91bf0791afe5719e9f755b48a0a8afe83b
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-13 08:29:40 +0800

    Add writing to output file implementation

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const { readFileSync, existsSync } = require('fs')
+const { readFileSync, existsSync, writeFileSync } = require('fs')
+const { argv } = require('process')
 
-if (existsSync(process.argv[2])) {
-  const codePerLine = readFileSync(process.argv[2], 'utf-8').split('\n')
-
-  for (let i = 0; i < codePerLine.length; i += 1) {
-    console.log(`${i + 1}: ${codePerLine[i]}`)
+if (existsSync(argv[2])) {
+  if (argv.length - 2 === 2 || argv.length - 2 === 3) {
+    const stringResult = readFileSync(argv[2], 'utf-8').split('\n').map((line, index) => `${index + 1}: ${line}`).join('\n')
+    if (existsSync(argv[3])) {
+      if (argv.length - 2 === 2 || argv[4] === '-n') {
+        console.log('Cannot overwrite existing output file.')
+      } else if (argv[4] === '-y') {
+        writeFileSync(argv[3], stringResult)
+      } else {
+        console.log('Invalid overwrite option')
+      }
+    } else {
+      writeFileSync(argv[3], stringResult)
+    }
+  } else {
+    console.log('Missing output file argument.')
   }
 } else {
-  console.log('File does not exist')
+  console.log('Input file does not exist.')
 }
