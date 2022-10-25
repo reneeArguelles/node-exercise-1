2520a362523a5cb5cfe4608e504367aecf5a5575
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-25 15:23:41 +0800

    Apply process.stdin to index.js program

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const { readFileSync, existsSync, writeFileSync } = require('fs')
 const [inputFile, outputFile, overwriteFlag] = process.argv.slice(2)
 
 if (!existsSync(inputFile)) {
-  console.log('Input file does not exist.')
-  process.exit(1)
+  let finalString = ''
+  process.stdin.on('readable', () => {
+    let chunk
+    // eslint-disable-next-line no-cond-assign
+    while ((chunk = process.stdin.read()) !== null) {
+      finalString += chunk.toString()
+    }
+    console.log(finalString.split('\n').map((line, index) => `${index + 1}: ${line}`).join('\n').slice(0, -1))
+  })
 } else if (outputFile === undefined) {
   console.log('Missing output file argument.')
   process.exit(1)
